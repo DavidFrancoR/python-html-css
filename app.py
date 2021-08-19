c0da78b191cb649ccf8649f003bf48a813d70908
DavidFrancoR <dfranco@franzier.com>
2021-08-19 23:06:49 +0000

    mensagem

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect, abort, render_template, flash
+
 #print("Hello world!")
+
+#configuração
+DATABASE = "blog.db"
+SECRET_KEY = 'pudim'
 
 app = Flask(__name__)
 
